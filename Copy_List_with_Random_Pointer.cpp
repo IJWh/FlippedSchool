4188d81f8983647bc66120b39b7f20538db3899a
IJWh <48313442+IJWh@users.noreply.github.com>
2020-07-24 17:51:42 +0900

    Update Copy_List_with_Random_Pointer.cpp

--- Copy_List_with_Random_Pointer.cpp
+++ Copy_List_with_Random_Pointer.cpp
@@ -56,3 +56,43 @@ public:
         return dc;
     }
 };
+
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head is None:
+            return None
+        dc = Node(head.val)
+        head_iter = head
+        dc_iter = dc
+        dict_hd = {}
+        dict_hd[head] = dc
+        
+        while head_iter.next is not None:
+            
+            head_iter = head_iter.next
+            dc_iter.next = Node(head_iter.val)
+            dc_iter = dc_iter.next
+            dict_hd[head_iter] = dc_iter
+            
+        
+        dc_iter = dc
+        head_iter = head
+        while dc_iter is not None:
+            if head_iter.random is not None:
+                dc_iter.random = dict_hd[head_iter.random]
+                
+            dc_iter = dc_iter.next
+            head_iter = head_iter.next
+        
+        
+        return dc
